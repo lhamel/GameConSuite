49b0ff09eedf4340a790ca5f4f77012517b85297
Laura Hamel <lhamel@gmail.com>
2011-11-22 22:42:25 -0500

    Moved U-Con specific configuration out of the base template to the configuration file.

--- config/config.php
+++ config/config.php
@@ -8,3 +8,9 @@ Role Playing, Collectible Card Games, Board Games,
 Miniatures, Train Games, Live Action RPGs, Video Games,
 Exhibitors' Hall, Auction, Specialty Tracks, and much more!
 EOD;
+
+
+/* Meta information for the site, appears in the headers of every page */
+$config['gcs']['meta']['defaultTitle'] = "U-Con Gaming Convention, Ann Arbor Michigan";
+$config['gcs']['meta']['keywords'] = "ucon, ann arbor, gaming convention, games, role-playing, collectable card games, ccgs, rpgs, rpga, magic, magic the gathering, auction, university of michigan, uofm, convention, miniatures, historicals, board games, card games";
+$config['gcs']['meta']['description'] = "Affordable gaming convention in Ann Arbor, MI featuring a large variety of games, exhibitors hall and auction. Games include Role-playing, RPGA, CCGs, board, miniatures, historicals, card games.";
