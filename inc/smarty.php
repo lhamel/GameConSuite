4588da42998f0f12f320d3405a9ba88dc4e1ad6f
lhamel <lhamel@gmail.com>
2020-01-23 23:19:46 -0500

    Updates to smarty lib configuration

--- inc/smarty.php
+++ inc/smarty.php
@@ -1,12 +1,10 @@
 <?php
-require_once dirname(__FILE__).'/lib/Smarty-2.6.26/libs/Smarty.class.php';
-//require_once dirname(__FILE__).'/../lib/Smarty-3.0b5/libs/Smarty.class.php';
+// TODO namespace / autoload
+require_once dirname(__FILE__).'/../vendor/smarty/smarty/libs/Smarty.class.php';
+
 $smarty = new Smarty();
 
-$smarty->template_dir = dirname(__FILE__).'/templates';
-$smarty->compile_dir = dirname(__FILE__).'/tmp/templates_c';
-$smarty->cache_dir = dirname(__FILE__).'/tmp/cache';
-$smarty->config_dir = dirname(__FILE__).'/tmp/config';
-
-//$smarty->assign('name', 'Ned');
-//$smarty->display('index.tpl');
+$smarty->template_dir = __DIR__.'/templates';
+$smarty->compile_dir = __DIR__.'/temp/smarty/templates_c';
+$smarty->cache_dir = __DIR__.'/temp/smarty/cache';
+$smarty->config_dir = __DIR__.'/temp/smarty/configs';
