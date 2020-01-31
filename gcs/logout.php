b04b523aab7b42d914f23fd1327f961c7d0a5593
lhamel <lhamel@gmail.com>
2020-01-30 21:20:39 -0500

    authentication user interface

---
+++ gcs/logout.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  Respond to a request to create an account
+ */
+require_once __DIR__.'/../inc/inc.php';
+require_once __DIR__.'/../inc/auth.php'; // TODO remove after moving to inc.php
+
+$year = YEAR;
+
+// if the user is currently logged in, this is an error
+if ($auth->isLogged()) {
+    $s = $auth->logout($auth->getSessionHash());
+}
+
+redirect('login.php');
+
+
