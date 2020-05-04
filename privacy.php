e518f5015d11c3b7c0424d34fb6d1409e4f71376
lhamel <lhamel@gmail.com>
2020-05-03 20:42:34 -0400

    Adding basic privacy and security pages.

---
+++ privacy.php
@@ -0,0 +1,32 @@
+<?php
+define('DEPTH','');
+require_once 'inc/inc.php';
+$year = $config['gcs']['year'];
+$location = 'privacy.php';
+
+require_once INC_PATH.'smarty.php';
+require_once INC_PATH.'layout/menu.php';
+
+ob_start();
+?>
+<h1>Privacy Policy</h1>
+
+<p>This website keeps record of names and addresses of those who have attended U-Con.  
+We do not store credit card or PayPal information, nor does this site ask for such information.  
+Records of all online payments are kept with PayPal.  We will not sell or trade 
+your personal information to third parties.</p>
+
+<a id="contact"><b>Questions?</b></a>
+
+<p>Should you have other questions or concerns about our privacy policy, please contact us via main website.</p>
+
+<?php
+$content = ob_get_contents();
+ob_clean();
+
+
+$smarty->assign('config', $config);
+$smarty->assign('content', $content);
+$smarty->display('base.tpl');
+
+
---
+++ security.php
@@ -0,0 +1,40 @@
+<?php
+define('DEPTH','');
+require_once 'inc/inc.php';
+$year = $config['ucon']['year'];
+$location = 'security.php';
+
+require_once INC_PATH.'smarty.php';
+require_once INC_PATH.'layout/menu.php';
+
+if (isset($_GET['cookiesdisabled']))
+        $special = '<p><font style="color: maroon">We are unabled to save your
+                        information because cookies have been disabled.</font></p>';
+else
+        $special = '';
+
+
+ob_start();
+?>
+
+<h1>Security Information</h1>
+
+<?php echo $special; ?>
+
+<p>This website uses session cookies in order to process registration and ticket
+orders.  Session cookies are small pieces of information stored on your
+computer during the process of preparing your order.  They are automatically
+removed when your web browser is closed, and thus cannot be used to track
+users between sessions.  If your browser is configured not to accept session
+cookies, you should still be able to browse the site, however you will be
+unable to register or purchase tickets.</p>
+<?php
+$content = ob_get_contents();
+ob_clean();
+
+
+$smarty->assign('config', $config);
+$smarty->assign('content', $content);
+$smarty->display('base.tpl');
+
+
