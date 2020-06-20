6c76741c2fb5031b1eb15da8c8438480d9c096ce
lhamel <lhamel@gmail.com>
2020-06-20 15:03:02 -0400

    Additional changes required for directory change

--- api/index.php
+++ api/index.php
@@ -95,8 +95,8 @@ $dbSettings = [
 ];
 
 
-require_once __DIR__.'/../../inc/db/db.php';
-require_once __DIR__.'/../../inc/auth.php';
+require_once __DIR__.'/../inc/db/db.php';
+require_once __DIR__.'/../inc/auth.php';
 
 $containerBuilder = new \DI\ContainerBuilder();
 
@@ -122,7 +122,7 @@ $containerBuilder->addDefinitions([
 $container = $containerBuilder->build();
 $router = new SlimRouter($container);
 $app = $router->getSlimApp();
-$app->setBasePath('/GameConSuite/api-def/out');
+$app->setBasePath('/GameConSuite/api');
 
 /**
  * The routing middleware should be added before the ErrorMiddleware
