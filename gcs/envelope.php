9b26ef50cfb28020df29f9ba4ac3538bac183379
lhamel <lhamel@gmail.com>
2020-07-11 21:49:23 -0400

    Move eventFormatter object to a JS file.

--- gcs/envelope.php
+++ gcs/envelope.php
@@ -522,58 +522,8 @@ $content .= <<< EOD
     </script>
 
 
+    <script src="{$config['page']['depth']}js/gcs/events.js"></script>
     <script>
-      var eventFormatter = {
-        capitalize: function(str) {
-          if (!str) { return str; }
-          return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
-        },
-        formatTitle: function(eventObj) {
-          if (!eventObj) {
-            console.error("Missing eventObj");
-          }
-          let name = eventObj.game;
-          if (eventObj.title) {
-            name += (name ? ': ' : '') + eventObj.title
-          }
-          return name;
-        },
-        formatGmObj: function(obj) {
-          if (!obj) { return '' };
-          let name = obj.lastName;
-          if (obj.firstName) {
-            name = obj.firstName + ' ' + name;
-          }
-          if (obj.groupName) {
-            name += ' (' + obj.groupName + ')';
-          }
-          return name;
-        },
-        formatPlayers: function(obj) {
-          if (!obj) { return '' };
-          let players = obj['maxplayers'];
-          let minplayers = obj['minplayers'];
-          if (minplayers && minplayers>0 && minplayers != players) {
-            players = minplayers + " - " + players;
-          }
-          return players;
-        },
-        formatSingleTime: function (time) {
-          if (!time) return "";
-          if (time < 12) return time+"a";
-          else if (time == 12) return "12p";
-          else if (time < 24) return (time-12)+"p";
-          else if (time == 24) return "12a";
-          else return (time-24)+"a";
-        },
-        formatTime: function(eventObj) {
-          let day = eventObj.day ? this.capitalize(eventObj.day) : '';
-          let time = eventObj.time ? eventObj.time : '';
-          let endtime = eventObj.endtime ? eventObj.endtime : '';
-          return day + (time? ' ' + this.formatSingleTime(time) + '-'+this.formatSingleTime(endtime)+' ET':'');
-        }
-      }
-
 
       Vue.component("schedule-list", {
         template: "#schedule-list-template",
