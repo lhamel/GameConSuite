375b3aba774296e7b4572a5f13262aeb23bb841e
lhamel <lhamel@gmail.com>
2020-10-17 15:17:59 -0400

    Attempt to bug fix the logout issue which occurs sporadically

--- gcs/logout.php
+++ gcs/logout.php
@@ -8,9 +8,9 @@ require_once __DIR__.'/../inc/auth.php'; // TODO remove after moving to inc.php
 $year = $config['gcs']['year'];
 
 // if the user is currently logged in, this is an error
-if ($auth->isLogged()) {
+// if ($auth->isLogged()) {
     $s = $auth->logout($auth->getCurrentSessionHash());
-}
+// }
 
 redirect('login.php');
 
